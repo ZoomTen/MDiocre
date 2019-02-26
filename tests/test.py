da1f90fbe7c879079f5eeaddc3044c2fae764780
ZoomTen <zy.impossible@gmail.com>
2019-02-26 08:32:18 +0000

    add unit test

---
+++ tests/test.py
@@ -0,0 +1,134 @@
+import sys
+import os
+
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
+import mdiocre
+import unittest
+
+class TestConfig(unittest.TestCase):
+    @classmethod
+    def setUpClass(TestConfig):
+        print("\nRunning mdiocre.Config test!")
+    
+    def setUp(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_load_config(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_load_config_file(self):
+        pass
+
+class TestWizard(unittest.TestCase):
+    @classmethod
+    def setUpClass(TestConfig):
+        print("\nRunning mdiocre.Wizard test!")
+    
+    def setUp(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_make_page(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_make_site(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_clean_site(self):
+        pass
+
+class TestUtils(unittest.TestCase):
+    @classmethod
+    def setUpClass(TestConfig):
+        print("\nRunning mdiocre.Utils test!")
+    
+    def setUp(self):
+        self.vars = {
+                    "test":"abc",
+                    "foo": "bar",
+                    "bar": "foo"
+                    }
+        self.utils = mdiocre.Utils(logger=mdiocre.Debug(False))
+    
+    @unittest.skip("todo")
+    def test_file_exists(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_file_does_not_exist_create(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_file_does_not_exist_nocreate(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_directory_exists(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_directory_does_not_exist_create(self):
+        pass
+    
+    @unittest.skip("todo")
+    def test_directory_does_not_exist_nocreate(self):
+        pass
+        
+    def test_variable_put_single(self):
+        text = "Hello from <!--var:test--> world"
+        proc = self.utils.process_vars(text, var_list=self.vars)
+        self.assertEqual(proc, "Hello from abc world")
+    
+    def test_variable_put_multiple(self):
+        text = "<!--var:test-->ing... <!--var:bar--><!--var:foo--> is <!--var:foo--> <!--var:bar-->"
+        proc = self.utils.process_vars(text, var_list=self.vars)
+        self.assertEqual(proc, "abcing... foobar is bar foo")
+
+    def test_variable_set_assign_string(self):
+        text = "Directly setting variable to string<!--var1=\"set\"-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Directly setting variable to string")
+        self.assertEqual(self.vars["var1"], "set")
+
+    def test_variable_set_assign_var(self):
+        text = "Directly assigning a variable to another<!--var3=test-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Directly assigning a variable to another")
+        self.assertEqual(self.vars["var3"], "abc")
+        
+    def test_variable_set_concat_var_str(self):
+        text = "Concatenating a variable and a string<!--var2=test,\" set\"-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Concatenating a variable and a string")
+        self.assertEqual(self.vars["var2"], "abc set")
+        
+    
+    def test_variable_set_concat_var_str_commaized(self):
+        text = "Concatenating a variable and a string<!--var2=test,\" set\, go\"-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Concatenating a variable and a string")
+        self.assertEqual(self.vars["var2"], "abc set, go")
+    
+    def test_variable_set_concat_vars_spaced(self):
+        text = "Concatenating two variables<!--var2=test, test-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Concatenating two variables")
+        self.assertEqual(self.vars["var2"], "abc abc")
+    
+    def test_variable_set_concat_vars_unspaced(self):
+        text = "Concatenating two variables<!--var2=test,test-->"
+        x = self.utils.process_vars(text, var_list=self.vars, set_var=True)
+        self.assertEqual(x,"Concatenating two variables")
+        self.assertEqual(self.vars["var2"], "abcabc")
+
+
+if __name__ == "__main__":
+   print("\n\nSTARTING UNIT TEST\n")
+   unittest.main(verbosity=2)

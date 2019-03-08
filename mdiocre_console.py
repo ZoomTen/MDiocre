e65e6ff2ef47b085a4ded7034914d26e5733f0e2
ZoomTen <zy.impossible@gmail.com>
2019-03-08 23:53:44 +0700

    argumentize app and site config, terrible hack

--- mdiocre_console.py
+++ mdiocre_console.py
@@ -25,7 +25,7 @@ def create_objects(args):
        log     = mdiocre.Debug(debug=args.detail, file_name=os.path.realpath(args.logfile))
     else:
        log     = mdiocre.Debug(debug=args.detail, file_name=None)
-    config     = mdiocre.Config(args.config, logger=log)
+    config     = mdiocre.Config(args.site_config, logger=log)
     wizard     = mdiocre.Wizard(config=config, logger=log)
 
 def build_site(args):
@@ -50,40 +50,16 @@ def none_mode(args):
     print("\nCurrently displaying set command line arguments:\n")
     print("="*10 + " GENERAL " + "="*10)
     print("Detail   (-d) :",args.detail)
-    print("Config   (-c) :",args.config)
+    print("App  Config   (-c) :",args.config)
+    print("Site Config   (-c) :",args.site_config)
     print("Log file (-l) :",args.logfile)
 
 def main():
-    import configparser as cp
-    config = cp.ConfigParser()
-    config.read("mdiocre.ini")
     # Default options
 
     loaded_opts = {"mdiocre": {"detail":False, "config":"config.ini", "logfile":None},\
                  "build":   {"include-html":False, "copy-html":False, "build-exclude":None, "use-prefix":True},\
                  "clean":   {"clean-exclude":None, "clean-index":True}}
-
-    # uh what
-    try: loaded_opts["mdiocre"]["detail"]  = config.getboolean("mdiocre", "detail")
-    except Exception: pass
-    try: loaded_opts["mdiocre"]["config"]  =        config.get("mdiocre", "config")
-    except Exception: pass
-    try: loaded_opts["mdiocre"]["logfile"] =        config.get("mdiocre", "logfile")
-    except Exception: pass
-
-    try: loaded_opts["build"]["include-html"]  = config.getboolean("build", "include-html")
-    except Exception: pass
-    try: loaded_opts["build"]["copy-html"]     = config.getboolean("build", "copy-html")
-    except Exception: pass
-    try: loaded_opts["build"]["build-exclude"] = config.get("build", "build-exclude")
-    except Exception: pass
-    try: loaded_opts["build"]["use-prefix"]    = config.getboolean("build", "use-prefix")
-    except Exception: pass
-
-    try: loaded_opts["clean"]["clean-exclude"]  = config.get("clean", "clean-exclude")
-    except Exception: pass
-    try: loaded_opts["clean"]["clean-index"]  = config.getboolean("clean", "clean-index")
-    except Exception: pass
 
     main_opts = loaded_opts["mdiocre"]
     build_opts = loaded_opts["build"]
@@ -94,7 +70,6 @@ def main():
               description = "Automated static website building tool using the"
                             " Markdown format."
              )
-
     p.add_argument(
                       "-v", "--version",
                       help="Display the version number",
@@ -105,16 +80,16 @@ def main():
                               +str(VERSION[2])+", "
                               +"released "+VERSION[3]
                       )
-    p.add_argument('-c', '--config',
-                       help='use custom configuration file')
+    p.add_argument('-C', '--config',
+                       help='use custom app configuration file')
+    p.add_argument('-c', '--site-config',
+                       help='use custom site configuration file')
     p.add_argument('-d', '--detail',
                        action='store_true',
                        help='display actions performed in more detail')
     p.add_argument('-l', '--logfile',
                        help='log action messages to a file in the root directory')
-    p.set_defaults(config=main_opts["config"],
-                   detail=main_opts["detail"],
-                   logfile=main_opts["logfile"], mode=None, run=none_mode)
+    
 
     p_subs  = p.add_subparsers()
 
@@ -142,11 +117,7 @@ def main():
                 action="store_true",
                 help="Use module prefixes in built files"
                 )
-    p_build.set_defaults(exclude=build_opts["build-exclude"],
-                         include_html=build_opts["include-html"],
-                         copy_html=build_opts["copy-html"],
-                         use_prefix=build_opts["use-prefix"],run=build_site, mode="building")
-
+    
     p_clean = p_subs.add_parser(
                   "clean",
                   help="clean built site")
@@ -159,16 +130,64 @@ def main():
                 action="store_true",
                 help="Also clean index.md files"
                 )
-    p_clean.set_defaults(exclude=clean_opts["clean-exclude"],
-                         clean_index=clean_opts["clean-index"],run=clean_site, mode="cleaning")
 
+    p.set_defaults(config="mdiocre.ini", mode=None, run=none_mode)
+    p_build.set_defaults(run=build_site, mode="building")
+    p_clean.set_defaults(run=clean_site, mode="cleaning")
+    
     # print help if invokes without commands
     if len(sys.argv)==1:
         p.print_help(sys.stderr)
         sys.exit(1)
+    
+    o = p.parse_args() # parse cmdline options
+    
+    import configparser as cp
+    config = cp.ConfigParser()
+    config.read(o.config)
+    # uh what
+    try: loaded_opts["mdiocre"]["detail"]  = config.getboolean("mdiocre", "detail")
+    except Exception: pass
+    try: loaded_opts["mdiocre"]["config"]  =        config.get("mdiocre", "config")
+    except Exception: pass
+    try: loaded_opts["mdiocre"]["logfile"] =        config.get("mdiocre", "logfile")
+    except Exception: pass
+
+    try: loaded_opts["build"]["include-html"]  = config.getboolean("build", "include-html")
+    except Exception: pass
+    try: loaded_opts["build"]["copy-html"]     = config.getboolean("build", "copy-html")
+    except Exception: pass
+    try: loaded_opts["build"]["build-exclude"] = config.get("build", "build-exclude")
+    except Exception: pass
+    try: loaded_opts["build"]["use-prefix"]    = config.getboolean("build", "use-prefix")
+    except Exception: pass
+
+    try: loaded_opts["clean"]["clean-exclude"]  = config.get("clean", "clean-exclude")
+    except Exception: pass
+    try: loaded_opts["clean"]["clean-index"]  = config.getboolean("clean", "clean-index")
+    except Exception: pass
+    
+    main_opts = loaded_opts["mdiocre"]
+    build_opts = loaded_opts["build"]
+    clean_opts = loaded_opts["clean"]
+    
+    p.set_defaults(config="mdiocre.ini",
+                   site_config=main_opts["config"],
+                   detail=main_opts["detail"],
+                   logfile=main_opts["logfile"], mode=None, run=none_mode)
+    p_build.set_defaults(exclude=build_opts["build-exclude"],
+                         include_html=build_opts["include-html"],
+                         copy_html=build_opts["copy-html"],
+                         use_prefix=build_opts["use-prefix"],run=build_site, mode="building")
+    p_clean.set_defaults(exclude=clean_opts["clean-exclude"],
+                         clean_index=clean_opts["clean-index"],run=clean_site, mode="cleaning")
 
     o = p.parse_args() # parse cmdline options
-
+    
+    #print("o contains", str(o))
+    #print("loaded opts contains", str(loaded_opts))
+    #exit()
+    
     create_objects(o)
 
     try:

82ec37d08f8795d0f0dc526cf6030cfe9e5b31fd
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-09-02 00:15:50 +0700

    bump version of 3.1a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020 Zumi'
 author = 'Zumi Daxuya'
 
 # The full version, including alpha/beta/rc tags
-release = '3.0.0'
+release = '3.1 alpha'
 
 
 # -- General configuration ---------------------------------------------------

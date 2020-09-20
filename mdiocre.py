6bda15c12d4533642035c266026eeaa91d9a4229
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2020-09-20 12:51:41 +0700

    cli: wrap in __main__

--- mdiocre.py
+++ mdiocre.py
@@ -6,25 +6,26 @@ APP_NAME    = 'MDiocre'
 APP_VERSION = '3.2a'
 APP_DATE    = '2020-09-15'
 
-ap = ArgumentParser(
-		description='A terrible static page generator.'
-	)
+if __name__ == '__main__':
+	ap = ArgumentParser(
+			description='A terrible static page generator.'
+		)
 
-ap.add_argument('source_dir', help='Webpages source directory')
-ap.add_argument('build_dir', help='Output directory')
-ap.add_argument('--quiet', '-q', help='No output', action='store_true')
+	ap.add_argument('source_dir', help='Webpages source directory')
+	ap.add_argument('build_dir', help='Output directory')
+	ap.add_argument('--quiet', '-q', help='No output', action='store_true')
 
-args = ap.parse_args()
+	args = ap.parse_args()
 
-w = Wizard(quiet=args.quiet)
+	w = Wizard(quiet=args.quiet)
 
-# display header
-if not args.quiet:
-	print()
-	app_str = "{} version {} ({})".format(APP_NAME, APP_VERSION, APP_DATE)
-	print(app_str)
-	print('='*len(app_str))
-	print()
+	# display header
+	if not args.quiet:
+		print()
+		app_str = "{} version {} ({})".format(APP_NAME, APP_VERSION, APP_DATE)
+		print(app_str)
+		print('='*len(app_str))
+		print()
 
-# run the wizard
-w.generate_from_directory(vars(args))
+	# run the wizard
+	w.generate_from_directory(vars(args))

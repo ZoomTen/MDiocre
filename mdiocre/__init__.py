866bb99b6e1ec9f73aeefb753a0a0e127e4bbfa3
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2022-06-29 18:48:54 +0700

    logging: rename levels to consistently have 4 characters

--- mdiocre/__init__.py
+++ mdiocre/__init__.py
@@ -10,22 +10,22 @@ for i in range(4):
 logging.addLevelName(logging.INFO+5, 'OK')
 for i in range(4):
 	i += 1
-	logging.addLevelName(logging.INFO+5+i, 'OK:{}'.format(i))
+	logging.addLevelName(logging.INFO+5+i, '-OK-:{}'.format(i))
 	
 #logging.addLevelName(logging.WARNING, 'WARNING')
 for i in range(4):
 	i += 1
-	logging.addLevelName(logging.WARNING+i, 'WARNING:{}'.format(i))
+	logging.addLevelName(logging.WARNING+i, 'WARN:{}'.format(i))
 
-logging.addLevelName(logging.WARNING+5, 'SERIOUS')
+logging.addLevelName(logging.WARNING+5, 'CRIT')
 for i in range(4):
 	i += 1
-	logging.addLevelName(logging.WARNING+5+i, 'SERIOUS:{}'.format(i))
+	logging.addLevelName(logging.WARNING+5+i, 'CRIT:{}'.format(i))
 
 #logging.addLevelName(logging.ERROR, 'ERROR')
 for i in range(4):
 	i += 1
-	logging.addLevelName(logging.ERROR+i, 'ERROR:{}'.format(i))
+	logging.addLevelName(logging.ERROR+i, 'ERR!:{}'.format(i))
 
 # import mdiocre modules
 from .core import MDiocre, VariableManager
